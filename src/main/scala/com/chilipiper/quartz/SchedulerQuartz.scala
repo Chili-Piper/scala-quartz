96e61554d556c9e12b374fd0fd25ec0d101142c4
Ondra Pelech <ondra@chilipiper.com>
2026-01-27 12:35:52 +0100

    Fix isDbInitialized

--- src/main/scala/com/chilipiper/quartz/SchedulerQuartz.scala
+++ src/main/scala/com/chilipiper/quartz/SchedulerQuartz.scala
@@ -149,7 +149,7 @@ object SchedulerQuartz {
     SELECT EXISTS (
       SELECT 1
       FROM information_schema.tables
-      WHERE table_schema = $schema AND table_name LIKE ${prefix + "%"}
+      WHERE table_schema = $schema AND LOWER(table_name) LIKE LOWER(${prefix + "%"})
     )
   """.query[Boolean].unique.transact(transactor)
 
